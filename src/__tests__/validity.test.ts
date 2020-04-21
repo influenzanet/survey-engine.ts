39d29c813d6d3460b62fef73eccd33f4aed36024
phev8 <phev8@users.noreply.github.com>
2020-04-21 21:28:55 +0200

    check validity for multiple survey items

--- src/__tests__/validity.test.ts
+++ src/__tests__/validity.test.ts
@@ -1,7 +1,7 @@
 import { SurveyEngineCore } from "../engine";
 import { Survey } from "../data_types";
 import { flattenSurveyItemTree } from "../utils";
-import { checkSurveyItemValidity } from "../validation-checkers";
+import { checkSurveyItemValidity, checkSurveyItemsValidity } from "../validation-checkers";
 
 
 test('testing validations', () => {
@@ -95,12 +95,6 @@ test('testing validations', () => {
     surveyE.setResponse('root.G1.1', {
         key: '1', items: [{ key: '1', items: [{ key: '1' }, { key: '2' }] }]
     });
-    surveyE.setResponse('root.G1.1', {
-        key: '1', items: [{ key: '1', items: [{ key: '1' }, { key: '2' }] }]
-    });
-    surveyE.setResponse('root.G2.1', {
-        key: '1', items: [{ key: '1', items: [{ key: '1' }, { key: '2' }] }]
-    });
     surveyE.setResponse('root.G2.1', {
         key: '1', items: [{ key: '1', items: [{ key: '1' }, { key: '2' }] }]
     });
@@ -118,8 +112,113 @@ test('testing validations', () => {
     vRes = checkSurveyItemValidity(i2);
     expect(vRes.hard).toBeTruthy();
     expect(vRes.soft).toBeTruthy();
+});
 
 
+test('testing multiple survey items validation', () => {
+    const testSurvey: Survey = {
+        current: {
+            surveyDefinition: {
+                key: "root",
+                version: 0,
+                items: [
+                    {
+                        key: 'root.G1', version: 1, selectionMethod: { name: 'sequential' }, items: [
+                            {
+                                key: 'root.G1.1', version: 1, validations: [
+                                    {
+                                        key: 'v1', type: 'soft', rule: {
+                                            name: 'responseHasKeysAny',
+                                            data: [
+                                                { str: 'root.G1.1' },
+                                                { str: '1.1' },
+                                                { str: '1' },
+                                            ]
+                                        }
+                                    },
+                                    {
+                                        key: 'v2', type: 'hard', rule: {
+                                            name: 'responseHasKeysAny',
+                                            data: [
+                                                { str: 'root.G1.1' },
+                                                { str: '1.1' },
+                                                { str: '2' },
+                                            ]
+                                        }
+                                    }
+                                ]
+                            },
+                        ]
+                    },
+                    {
+                        key: 'root.G2', version: 1, items: [
+                            {
+                                key: 'root.G2.1', version: 1,
+                                validations: [
+                                    {
+                                        key: 'v1', type: 'hard', rule: {
+                                            name: 'responseHasKeysAny',
+                                            data: [
+                                                { str: 'root.G2.1' },
+                                                { str: '1.1' },
+                                                { str: '1' },
+                                            ]
+                                        }
+                                    },
+                                    {
+                                        key: 'v2', type: 'hard', rule: {
+                                            name: 'responseHasKeysAny',
+                                            data: [
+                                                { str: 'root.G2.1' },
+                                                { str: '1.1' },
+                                                { str: '2' },
+                                            ]
+                                        }
+                                    }
+                                ]
+                            },
+                        ]
+                    },
+                ],
+            }
+        },
+    };
 
+
+    const surveyE = new SurveyEngineCore(
+        testSurvey,
+    );
+
+    let items = flattenSurveyItemTree(surveyE.getRenderedSurvey());
+    let vRes = checkSurveyItemsValidity(items);
+    expect(vRes.hard).toBeFalsy();
+    expect(vRes.soft).toBeFalsy();
+
+    surveyE.setResponse('root.G1.1', {
+        key: '1', items: [{ key: '1', items: [{ key: '1' }, { key: '2' }] }]
+    });
+
+    items = flattenSurveyItemTree(surveyE.getRenderedSurvey());
+    vRes = checkSurveyItemsValidity(items);
+    expect(vRes.hard).toBeFalsy();
+    expect(vRes.soft).toBeTruthy();
+
+    surveyE.setResponse('root.G1.1', {
+        key: '1', items: [{ key: '1', items: [{ key: '1' }, { key: '2' }] }]
+    });
+
+    items = flattenSurveyItemTree(surveyE.getRenderedSurvey());
+    vRes = checkSurveyItemsValidity(items);
+    expect(vRes.hard).toBeFalsy();
+    expect(vRes.soft).toBeTruthy();
+
+    surveyE.setResponse('root.G2.1', {
+        key: '1', items: [{ key: '1', items: [{ key: '1' }, { key: '2' }] }]
+    });
+
+    items = flattenSurveyItemTree(surveyE.getRenderedSurvey());
+    vRes = checkSurveyItemsValidity(items);
+    expect(vRes.hard).toBeTruthy();
+    expect(vRes.soft).toBeTruthy();
 });
 
--- src/validation-checkers.ts
+++ src/validation-checkers.ts
@@ -29,3 +29,24 @@ export const checkSurveyItemValidity = (item: SurveySingleItem): ValidityResults
     });
     return results;
 }
+
+export const checkSurveyItemsValidity = (items: SurveySingleItem[]): ValidityResults => {
+    const results = {
+        soft: true,
+        hard: true
+    };
+
+    for (let item of items) {
+        const itemValid = checkSurveyItemValidity(item);
+        if (!itemValid.soft) {
+            results.soft = false;
+        }
+        if (!itemValid.hard) {
+            results.hard = false;
+        }
+        if (!results.soft && !results.hard) {
+            break;
+        }
+    }
+    return results;
+}
